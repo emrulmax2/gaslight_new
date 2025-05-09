a707265783f8d53730d920db35828303ecb252c2
K M Abdul Latif <143424592+kmabdullatif@users.noreply.github.com>
2025-05-09 22:31:57 +0600

    Wbhook Pertial + Company Address
    Wbhook Pertial + Company Address

--- app/Http/Controllers/UserSubscriptionWebhookController.php
+++ app/Http/Controllers/UserSubscriptionWebhookController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserPricingPackage;
+use App\Models\UserPricingPackageInvoice;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Stripe\Exception\SignatureVerificationException;
+use Stripe;
 
 class UserSubscriptionWebhookController extends Controller
 {
@@ -23,15 +27,77 @@ class UserSubscriptionWebhookController extends Controller
             return response()->json(['error' => 'Invalid signature'], 400);
         }
 
+        $message = 'Noting Found!';
         // Handle the event
         switch ($event->type) {
             case 'invoice.payment_failed':
-                $theObject = $event->data->object; // contains a \Stripe\PaymentIntent
-                Log::info(json_encode($theObject->id));
+                $theObject = $event->data->object;
+                //Log::info(json_encode($theObject->id));
+                $invoice_id = $theObject->id;
+                $customer_id = $theObject->customer;
+                $subscription_id = $theObject->parent->subscription_details->subscription;
+                $user_id = $theObject->parent->subscription_details->metadata->user_id;
+
+                $userPackage = UserPricingPackage::where('stripe_customer_id', $customer_id)->orderBy('id', 'DESC')->get()->first();
+                if(!isset($userPackage->id) && !empty($subscription_id)):
+                    $userPackage = UserPricingPackage::where('stripe_subscription_id', $subscription_id)->orderBy('id', 'DESC')->get()->first();
+                elseif(!isset($userPackage->id) && !empty($user_id)):
+                    $userPackage = UserPricingPackage::where('user_id', $user_id)->orderBy('id', 'DESC')->get()->first();
+                endif;
+                if(isset($userPackage->id) && $userPackage->id > 0):
+                    $userPackage->update([
+                        'active' => 0,
+                        'updated_by' => Auth::user()->id
+                    ]);
+                    $userInvoice = UserPricingPackageInvoice::updateOrCreate(['user_pricing_package_id' => $userPackage->id, 'invoice_id' => $invoice_id], [
+                        'user_id' => $userPackage->user_id,
+                        'user_pricing_package_id' => $userPackage->id,
+                        'invoice_id' => $invoice_id,
+                        'start' => date('Y-m-d', strtotime($theObject->period_start)),
+                        'end' => date('Y-m-d', strtotime($theObject->period_end)),
+                        'status' => 'incomplete',
+                        
+                        'created_by' => Auth::user()->id,
+                    ]);
+
+                    $message = 'Subscription failed status successfully updated.';
+                endif;
                 break;
             case 'invoice.payment_succeeded':
                 $theObject = $event->data->object; // contains a \Stripe\PaymentMethod
                 Log::info(json_encode($theObject));
+                $invoice_id = $theObject->id;
+                $customer_id = $theObject->customer;
+                $subscription_id = $theObject->parent->subscription_details->subscription;
+                $user_id = $theObject->parent->subscription_details->metadata->user_id;
+
+                $userPackage = UserPricingPackage::where('stripe_customer_id', $customer_id)->orderBy('id', 'DESC')->get()->first();
+                if(!isset($userPackage->id) && !empty($subscription_id)):
+                    $userPackage = UserPricingPackage::where('stripe_subscription_id', $subscription_id)->orderBy('id', 'DESC')->get()->first();
+                elseif(!isset($userPackage->id) && !empty($user_id)):
+                    $userPackage = UserPricingPackage::where('user_id', $user_id)->orderBy('id', 'DESC')->get()->first();
+                endif;
+                if(isset($userPackage->id) && $userPackage->id > 0):
+                    $userPackage->update([
+                        'active' => 0,
+                        'start' => date('Y-m-d', strtotime($theObject->period_start)),
+                        'end' => date('Y-m-d', strtotime($theObject->period_end)),
+                        'price' => ($theObject->total / 100),
+                        'updated_by' => Auth::user()->id
+                    ]);
+                    $userInvoice = UserPricingPackageInvoice::updateOrCreate(['user_pricing_package_id' => $userPackage->id, 'invoice_id' => $invoice_id], [
+                        'user_id' => $userPackage->user_id,
+                        'user_pricing_package_id' => $userPackage->id,
+                        'invoice_id' => $invoice_id,
+                        'start' => date('Y-m-d', strtotime($theObject->period_start)),
+                        'end' => date('Y-m-d', strtotime($theObject->period_end)),
+                        'status' => 'incomplete',
+                        
+                        'created_by' => Auth::user()->id,
+                    ]);
+
+                    $message = 'Subscription failed status successfully updated.';
+                endif;
                 break;
             case 'customer.subscription.deleted':
                 $theObject = $event->data->object; 
@@ -42,7 +108,7 @@ class UserSubscriptionWebhookController extends Controller
                 break;
         }
 
-        return response()->json(['message' => 'Success!'], 200);
+        return response()->json(['message' => $message], 200);
     }
 }
 
